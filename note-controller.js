85a703b5e7e70dfe0ad8bc8e7db3285209a4258f
Lucas Salmins <lucasasalmins@gmail.com>
2017-11-08 16:58:44 +0000

    added links + some spiking

--- note-controller.js
+++ note-controller.js
@@ -1,35 +1,41 @@
-(function(exports){
+(function(exports) {
 
-var _noteList = new NoteList();
+  var _noteList = new NoteList();
 
-var addDiv = function(){
-  var div = document.createElement("div");
-  document.body.appendChild(div);
-  return div;
-}
-var appendTextToElement = function(text, elem ){
-  var docText = document.createTextNode(text);
-  elem.appendChild(docText);
-}
+  var addDiv = function() {
+    var div = document.createElement("div");
+    // document.body.appendChild(div);
+    document.getElementById("abbrv").appendChild(div)
+    return div;
+  }
+  var addClass = function(className, elem) {
+    elem.setAttribute('class', className);
+  }
 
-var addClass = function(className, elem){
-  elem.setAttribute('class', className); 
-}
+  var addHTMLtoNoteText = function(text, index) {
+    // return "<a onclick='openNoteFunction()'>" + text + "</a>";
+    return "<a href='javascript:openNoteFunction(" + index + ")'>" + text + "</a>";
+  }
 
-var createNote = function(){
-  text = document.getElementById("newNote").value
-  _noteList.createNote(text);
-  addNote();
-}
+  var openNoteFunction = function() {
+    alert("hello")
+  }
 
-var addNote = function (){
-  var note = addDiv(); // creates div
-  appendTextToElement(_noteList.getLastNoteText(), note); //adds text
-  addClass("notediv", note); //adds class 
-  var notediv = document.getElementsByClassName("notediv");
-  notediv[notediv.length-1].innerHTML =  _noteList.getLastNoteText();
-}
+  var addNote = function() {
+    var note = addDiv(); // creates div
+    addClass("notediv", note); //adds class
+    var notediv = document.getElementsByClassName("notediv");
+    notediv[notediv.length - 1].innerHTML = addHTMLtoNoteText(_noteList.getLastNoteText(), _noteList.getLastNoteIndex());
+    console.log(_noteList);
+  }
 
-exports.createNote = createNote;
+  var createNote = function() {
+    text = document.getElementById("newNote").value
+    _noteList.createNote(text);
+    addNote();
+  }
+
+  exports.createNote = createNote;
+  exports.openNoteFunction = openNoteFunction;
+
 })(this);
-
